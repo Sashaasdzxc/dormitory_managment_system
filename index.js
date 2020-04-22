5b46d0c3e4709f2d0e6f33296f68d9ede3b1cadc
Alexandr Vesyolkin <sashaasdzxc@gmail.com>
2020-04-22 11:38:32 +0300

    Made server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("nothing");
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res) {
+    res.sendFile(__dirname + "/html/control.html");
+})
+app.listen(3000);
