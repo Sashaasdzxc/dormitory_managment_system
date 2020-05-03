1de3f85c59668afa0ec9bc2546a474f2430fb336
Alexandr Vesyolkin <sashaasdzxc@gmail.com>
2020-05-03 14:34:32 +0300

    ya

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
-app.use(express.static(__dirname + '/public'));
-app.get('/',function(req,res) {
-    res.sendFile(__dirname + "/html/control.html");
+app.set('view engine', 'hbs');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Dormitory'
 });
-app.get('/st',function(req,res) {
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    connection.query("SELECT * FROM students",
+        function (err, results, fields) {
+            res.render('control', { users: results });
+        });
+    connection.end();
+});
+app.get('/st', function (req, res) {
     res.sendFile(__dirname + "/html/st.html");
 })
-app.get('/z',function(req,res) {
+app.get('/z', function (req, res) {
     res.sendFile(__dirname + "/html/zayavka.html");
 })
 app.listen(3000);
