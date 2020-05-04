90c19c8571e12c95c21471874b244d2e24d23bf5
Alexandr Vesyolkin <sashaasdzxc@gmail.com>
2020-05-04 12:13:32 +0300

    add delete func

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+const bodyParser = require("body-parser");
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.set('view engine', 'hbs');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -8,14 +11,25 @@ var connection = mysql.createConnection({
     password: '',
     database: 'Dormitory'
 });
+
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
     connection.query("SELECT * FROM students",
         function (err, results, fields) {
             res.render('control', { users: results });
         });
-   // connection.end();
+    // connection.end();
 });
+
+app.post("/delete/:id", function (req, res) {
+    const id = req.params.id;
+    connection.query("SELECT * FROM students WHERE id=?", [id], function (err, result, fields) {
+        //console.log(result);
+        res.redirect("/");
+    });
+});
+
 app.get('/st', function (req, res) {
     res.sendFile(__dirname + "/html/st.html");
 })
